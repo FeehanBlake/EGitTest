792689239be9c4cda433246fb90d18e428650ed2
Blake Feehan <feehanb@ferris.edu>
2026-06-24 19:43:42 -0400

    Initial Commit

---
+++ eGitTest/src/eGitTestPkg/Test1.java
@@ -0,0 +1,11 @@
+package eGitTestPkg;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("This is an eGit Test!");
+
+	}
+
+}
